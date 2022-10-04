6129a9fbbb77b25e61cebeb2521d63127da87380
Alina Shtramwasser <Alina.Shtramwasser@computershare.com>
2022-10-04 11:08:19 -0400

    more cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,25 @@ import {ToastModule} from 'primeng/toast'
 })
 export class AppComponent {
   title = 'Winery Log';
+  //modifyTriggered = false;
+  //showList = true;
 
   constructor(private messageService: MessageService){
     
   }
+//listening to the addClicked event as explained on https://angular.io/guide/component-interaction, parent listening to child event, shows the add and hides the main view
+  // onModifyClicked(event){
+  //   alert(event.id);
+  //   this.setBooleans(false);
+  // }
+
+  //Listening to the SaveClicked event - will show the main view and hide the add
+  onSaveClicked(){
+   // this.setBooleans(true);
+  }
+  
+  // setBooleans(visibility: boolean){
+  //   this.modifyTriggered = !visibility;
+  //   this.showList = visibility;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import {InputTextModule} from 'primeng/inputtext';
 import {InputMaskModule} from 'primeng/inputmask';
-import { CommonModule } from '@angular/common';
+//import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import {PanelModule} from 'primeng/panel';
 //for fetching data from external apis and provide them to the app as a stream
 import { HttpClientModule } from "@angular/common/http";
 import {TableModule} from 'primeng/table';
-import { WineryGridComponent } from './winery-grid/winery-grid.component';
+//import { WineryGridComponent } from './winery-grid/winery-grid.component';
 import { WineryService } from './winery.service';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
@@ -22,14 +22,14 @@ import {VirtualScrollerModule} from 'primeng/virtualscroller';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import {RatingModule} from 'primeng/rating';
-import { AddWineryComponentComponent } from './addWinery-component/addWinery-component.component';
+import { ModifyWineryComponent } from './modify-winery/modify-winery.component';
 import {ButtonModule} from 'primeng/button';
 @NgModule({
   declarations: [							
     AppComponent,
-    WineryGridComponent,
+    //WineryGridComponent,
     WineryListComponent,
-      AddWineryComponentComponent
+    ModifyWineryComponent
    ],
   imports: [
     BrowserModule,
@@ -41,7 +41,14 @@ import {ButtonModule} from 'primeng/button';
     VirtualScrollerModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: "", component: WineryGridComponent }
+      { path: "", component: WineryListComponent },
+      {path: 'wineries', component: WineryListComponent},
+     // { path: 'wineries/:id', component: ModifyWineryComponent },
+      {
+        path: 'wineries/:id/edit',
+      //  canDeactivate: [ProductEditGuard],
+        component: ModifyWineryComponent
+      }
     ]),
     TableModule,
     ToastModule,
