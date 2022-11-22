3336bb47300d32f8f2b7c7f6c8c74fb2c9a53577
Alina Shtramwasser <Alina.Shtramwasser@computershare.com>
2022-11-22 16:59:20 -0500

    added the email and login name surrounded by a light div

--- src/app/winery-list/winery-list.component.ts
+++ src/app/winery-list/winery-list.component.ts
@@ -3,8 +3,8 @@ import { MessageService } from 'primeng/api';
 import { Subscription } from 'rxjs';
 import { DialogService } from '../dialog.service';
 import { WineryService } from '../winery.service';
-import {PasswordModule} from 'primeng/password';
-
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 @Component({
 	selector: 'app-winery-list',
 	templateUrl: './winery-list.component.html',
@@ -14,24 +14,23 @@ import {PasswordModule} from 'primeng/password';
 export class WineryListComponent implements OnInit, OnDestroy {
 
 	private getAllSubscription: Subscription | undefined;
+	loginForm: FormGroup;
 	codeName = 'winery-grid';
 	wineries = [];
-    isLoggedin?: boolean;
+	isLoggedin?: boolean;
+	emailErrorMessage: string;
 	totalRecords: number = 0;
 	alternativeUrl: string = "assets/images/wineryLogo.jpg"
 	/*
 	** On component creation (inject services).
 	*/
 	constructor(
+		private fb: FormBuilder,
 		private _messageService: MessageService,
 		// to talk to the web server
 		private _data: WineryService,
 		private _dialoService: DialogService
 	) { }
-
-	login(){
-		this.isLoggedin = true;
-	}
 
 	/*
 	** On component initialization, get all data from the data service.
@@ -40,6 +39,41 @@ export class WineryListComponent implements OnInit, OnDestroy {
 		// load all records
 		this.getAllWineries();
 		this.isLoggedin = false;
+		//setting up all the fields and validations
+		this.loginForm = this.fb.group({
+			googlePassword: ['', Validators.required],
+			googleEmail: ['', [Validators.required, Validators.email]],
+		});
+
+
+		const loginEmailControl = this.loginForm.get('googleEmail');
+		loginEmailControl.valueChanges.pipe(
+			debounceTime(1000)
+		).subscribe(
+			value => this.setMessage(loginEmailControl, 'googleEmail')
+		);
+	}
+
+	login(): void {
+		//validation is OK
+		if (this.loginForm.valid) {
+			//changes exist
+			if (this.loginForm.dirty) {
+				this.isLoggedin = true;
+
+				var email = this.loginForm.get("googleEmail").value;
+				alert(email);
+				var password = this.loginForm.get("googlePassword").value;
+				alert(password);
+			}
+		}
+	}
+
+	setMessage(control: AbstractControl, name: string): void {
+		this.emailErrorMessage = '';
+		if ((control.touched || control.dirty) && control.errors) {
+			this.emailErrorMessage = "Please enter a valid email";
+		}
 	}
 
 	ngOnDestroy(): void {
